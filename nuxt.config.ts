9057500f92aa8502cee644ea35592bd4769d86ff
ssogle <sobang91@gmail.com>
2024-04-08 00:31:39 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
+const path = require('path')
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   app: {
+    baseURL: '/somcandy_house/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     head: {
       title: 'somcandy_house',
+    },
+  },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, 'docs'),
     },
   },
   // modules: ['@nuxtjs/tailwindcss', 'nuxt-quasar-ui'],
