a8a58f2e96badea0342ea8db8da521eff4fddf73
ssogle <sobang91@gmail.com>
2024-04-07 23:21:30 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'somcandy_house',
+    },
+  },
+  // modules: ['@nuxtjs/tailwindcss', 'nuxt-quasar-ui'],
+  // quasar: {
+  // iconSet: 'mdi-v7',
+  // },
+})
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="tw-w-dvw tw-h-dvh tw-flex tw-justify-center tw-items-center tw-flex-col tw-bg-[#ffc0b9] tw-p-10"
+  >
+    <div
+      class="tw-flex tw-justify-center tw-bg-[#fff] tw-items-center tw-w-[200px] tw-h-[200px] tw-border tw-shadow-md tw-rounded-full tw-mb-2"
+    >
+      <img src="~/assets/imgs/avatar001.png" class="tw-w-full tw-h-full" />
+    </div>
+
+    <div
+      class="tw-flex tw-flex-col tw-gap-0 tw-justify-center tw-items-center tw-text-white tw-mb-10"
+    >
+      <div class="tw-font-bold tw-text-lg">somcandy_house</div>
+      <div>솜캔디 | 이모티콘 작가</div>
+    </div>
+
+    <div class="tw-flex tw-flex-col tw-gap-4 tw-w-full tw-max-w-[300px]">
+      <a
+        href="https://ogqmarket.naver.com/creators/%EC%86%9C%EC%BA%94%EB%94%94Somcandy"
+        class="btn"
+        target="_blank"
+        >네이버 OGQ 마켓</a
+      >
+      <a
+        href="https://blog.naver.com/somcandy_house"
+        class="btn"
+        target="_blank"
+      >
+        네이버 블로그 - 무료 배경화면
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  @apply tw-p-4 tw-bg-[#fff] tw-shadow-md tw-rounded tw-text-center tw-text-gray-500;
+}
+</style>
