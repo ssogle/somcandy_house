d6cac8e7029c74a260094d68ff3a0f2a08a7741a
ssogle <sobang91@gmail.com>
2026-04-07 23:34:49 +0900

    fix: 링크 수정

--- pages/index.vue
+++ pages/index.vue
@@ -23,21 +23,18 @@
         <img src="~/assets/imgs/instagram.svg" class="tw-w-[16px]" />
         somcandy_house</a
       >
-      <div>솜캔디 | 이모티콘 작가</div>
+      <div>솜캔디하우스</div>
     </div>
 
     <div class="tw-flex tw-flex-col tw-gap-4 tw-w-full tw-max-w-[300px]">
+      <a href="https://forms.gle/MZLLx7e3Bb7enaWb6" class="btn" target="_blank"
+        >사랑둥이 아기씨 키링 주문하기</a
+      >
       <a
         href="https://ogqmarket.naver.com/creators/%EC%86%9C%EC%BA%94%EB%94%94Somcandy"
         class="btn"
         target="_blank"
         >네이버 OGQ 마켓</a
-      >
-      <a
-        href="https://store.line.me/stickershop/author/3372710/ko"
-        class="btn"
-        target="_blank"
-        >LINE STORE</a
       >
       <a
         href="https://blog.naver.com/somcandy_house"
