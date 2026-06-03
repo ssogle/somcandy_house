06f3839d280bec4a68ddcc79f70c1d607f38a88f
ssogle <sobang91@gmail.com>
2026-06-04 02:50:23 +0900

    feat: 노른자톡 판매 버튼 추가 및 스타일 수정

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,25 @@
 
 <template>
   <div
-    class="tw-w-dvw tw-h-dvh tw-flex tw-justify-center tw-items-center tw-flex-col tw-bg-[#ffc0b9] tw-p-10"
+    class="tw-w-dvw tw-h-dvh tw-flex tw-justify-center tw-items-center tw-flex-col tw-bg-[#ffe7d2] tw-p-10"
   >
     <a
       href="https://www.instagram.com/somcandy_house/?igsh=czZzdnB3c2Jwa2ww"
       target="_blank"
-      class="tw-flex tw-justify-center tw-bg-[#fff] tw-items-center tw-w-[200px] tw-h-[200px] tw-border tw-shadow-md tw-rounded-full tw-mb-2"
+      class="tw-flex tw-justify-center tw-bg-[#ffe7d2] tw-items-center tw-w-[200px] tw-mb-6"
     >
-      <img src="~/assets/imgs/avatar001.png" class="tw-w-full tw-h-full" />
+      <img src="~/assets/imgs/avatar002.png" class="tw-w-full tw-h-full" />
     </a>
 
     <div
-      class="tw-flex tw-flex-col tw-gap-0 tw-justify-center tw-items-center tw-text-white tw-mb-10"
+      class="tw-flex tw-flex-col tw-gap-0 tw-justify-center tw-items-center tw-text-gray-700 tw-mb-6"
     >
       <a
         href="https://www.instagram.com/somcandy_house/?igsh=czZzdnB3c2Jwa2ww"
         target="_blank"
         class="tw-font-bold tw-text-lg tw-underline tw-flex tw-gap-1 tw-justify-center tw-items-center"
       >
-        <img src="~/assets/imgs/instagram.svg" class="tw-w-[16px]" />
+        <img src="~/assets/imgs/instagram-black.svg" class="tw-w-[16px]" />
         somcandy_house</a
       >
       <div>솜캔디하우스</div>
@@ -30,6 +30,9 @@
       <!-- <a href="https://forms.gle/MZLLx7e3Bb7enaWb6" class="btn" target="_blank"
         >사랑둥이 아기씨 키링 주문하기</a
       > -->
+      <a href="https://forms.gle/fhXRwo2f6vRbMe3j8" class="btn" target="_blank"
+        >노른자톡 주문하기</a
+      >
       <a
         href="https://ogqmarket.naver.com/creators/%EC%86%9C%EC%BA%94%EB%94%94Somcandy"
         class="btn"
